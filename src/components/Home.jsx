709fddb70e0d51985ef60321d2a4422b21d6db98
Othniel <chukchi2345@gmail.com>
2024-12-25 23:35:01 +0100

    Modified Home section, added Texts and profile image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,39 @@
+import plusSigns from "./../assets/Image-Design/Design-1.svg";
+import profilePhoto from "./../assets/Image-Design/Profile-Photo.png";
+
 const Home = () => {
   return (
     <>
-      <main id="Home" style={{ scrollMarginTop: '80px' }} className="container mx-auto h-64 bg-slate-400 flex justify-center items-center">
-        <h1 className="text-center text-5xl font-extrabold">Home</h1>
+      <main
+        id="Home"
+        style={{ scrollMarginTop: "80px" }}
+        className="flex flex-col-reverse justify-between items-center lg:flex-row px-7 pt-8"
+      >
+        <section>
+          <p className="text-1xl lg:text-2xl">
+            Hello, I am
+            <strong> Chukwuani Chinonso Othniel</strong>
+          </p>
+          <h1 className="text-3xl lg:text-8xl">
+            Front-end <br /> Web Developer
+          </h1>
+          <p className=" text-1xl lg:text-2xl mt-5">
+            {" "}
+            +1 Year Experience. Based in Enugu City, Nigeria.
+          </p>
+        </section>
+        <section className=" w-[80%] lg:w-96">
+          <img
+            src={profilePhoto}
+            alt=""
+            className="rounded-full relative right-7"
+          />
+          <img
+            src={plusSigns}
+            alt=""
+            className="relative bottom-16 lg:right-9"
+          />
+        </section>
       </main>
     </>
   );
