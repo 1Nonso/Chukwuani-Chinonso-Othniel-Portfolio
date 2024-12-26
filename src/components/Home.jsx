6dc605c5f0b34614cd51643e1a200852970dc5d0
Othniel <chukchi2345@gmail.com>
2024-12-26 20:43:45 +0100

    Modified home section, added a text and some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,31 +7,34 @@ const Home = () => {
       <main
         id="Home"
         style={{ scrollMarginTop: "80px" }}
-        className="flex flex-col-reverse justify-between items-center lg:flex-row px-7 pt-8"
+        className="flex flex-col justify-between items-center lg:flex-col px-7 pt-8"
       >
-        <section>
-          <p className="text-1xl lg:text-2xl">
-            Hello, I am
-            <strong> Chukwuani Chinonso Othniel</strong>
-          </p>
-          <h1 className="text-3xl lg:text-8xl">
-            Front-end <br /> Web Developer
-          </h1>
-          <p className=" text-1xl lg:text-2xl mt-1 lg:mt-5">
-            +1 Year Experience. Based in Enugu City, Nigeria.
-          </p>
-        </section>
-        <section className=" w-[80%] md:w-[50%] lg:w-96">
-          <img
-            src={profilePhoto}
-            alt=""
-            className="rounded-full relative left-0 lg:right-6"
-          />
-          <img
-            src={plusSigns}
-            alt=""
-            className="relative bottom-6 right-9 lg:right-9"
-          />
+        <p className=" text-2xl md:text-5xl mb-5 welcome">Welcome</p>
+        <section className="flex flex-col-reverse justify-between items-center lg:flex-row px-7 pt-8 w-full">
+          <div>
+            <p className="text-1xl lg:text-2xl">
+              Hello, I'm
+              <strong> Chukwuani Chinonso Othniel,</strong>
+            </p>
+            <h1 className="text-3xl lg:text-8xl">
+              Front-end <br /> Web Developer
+            </h1>
+            <p className=" text-1xl lg:text-2xl mt-1 lg:mt-5">
+              +1 Year Experience. Based in Enugu City, Nigeria.
+            </p>
+          </div>
+          <div className="mb-4 sm:mb-0 w-[80%] md:w-[50%] lg:w-96">
+            <img
+              src={profilePhoto}
+              alt="Image of Myself"
+              className="rounded-full -z-50 relative left-0 lg:right-6"
+            />
+            <img
+              src={plusSigns}
+              alt="plus Signs"
+              className="-z-50 relative bottom-6 right-9 lg:right-9"
+            />
+          </div>
         </section>
       </main>
     </>
