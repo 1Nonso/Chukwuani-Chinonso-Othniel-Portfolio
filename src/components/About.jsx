5215cf93524957efffcab522879018ac04331740
Othniel <chukchi2345@gmail.com>
2024-12-27 10:24:46 +0100

    Modified About section, added my tech stack section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,9 @@
+import htmlImage from "./../assets/html5-logo.svg";
+import cssImage from "./../assets/css3-logo.svg";
+import jsImage from "./../assets/javascript-logo.svg";
+import reactLogo from "./../assets/react-svgrepo-com.svg";
+import tailwindLogo from "./../assets/tailwind-logo.svg";
+
 const About = () => {
   const projectEperience = [
     {
@@ -9,6 +15,44 @@ const About = () => {
       id: 2,
       title: "+1",
       description: "Year Experience",
+    },
+  ];
+
+  const techStack = [
+    {
+      id: 1,
+      img: htmlImage,
+      name: "HTML (HyerText Markup Language)",
+      classification: "User Interface",
+      description: "This is web-based technology used in adding elements on to the browser",
+    },
+    {
+      id: 2,
+      img: cssImage,
+      name: "CSS (Cascading Style Sheet)",
+      classification: "User Interface",
+      description: "",
+    },
+    {
+      id: 3,
+      img: jsImage,
+      name: "Javascript",
+      classification: "User Experience/ Interactivity",
+      description: "",
+    },
+    {
+      id: 4,
+      img: reactLogo,
+      name: "React",
+      classification: "User Experience",
+      description: "",
+    },
+    {
+      id: 5,
+      img: tailwindLogo,
+      name: "Tailwindcss",
+      classification: "User Interface",
+      description: "",
     },
   ];
 
@@ -33,17 +77,43 @@ const About = () => {
             {projectEperience.map((project) => {
               const { id, title, description } = project;
               return (
-                <>
-                  <div key={id} className="flex flex-col items-start">
-                    <p className="text-4xl">{title}</p>
-                    <p>{description}</p>
-                  </div>
-                </>
+                <div key={id} className="flex flex-col items-start">
+                  <p className="text-4xl">{title}</p>
+                  <p>{description}</p>
+                </div>
               );
             })}
           </section>
         </section>
-        <section></section>
+        <section className="mt-4 px-1 w-full">
+          <p className="text-xl">Powered By</p>
+          <div className="my-6">
+            <p className="my-1">My Tech Stack:</p>
+            <div className="techStack grid-cols-1 md:grid-cols-2 gap-8 grid-rows-2 w-full">
+              {techStack.map((tech) => {
+                const { id, img, name, classification } = tech;
+                return (
+                  <div
+                    key={id}
+                    className="flex flex-row items-center w-[22rem]"
+                  >
+                    <img src={img} alt="" className="w-[10%] mx-1" />
+                    <div className="w-[90%]">
+                      <p>
+                        <strong>{name}</strong>
+                      </p>
+                      <p>{classification}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div>
+            <p>Other Tools:</p>
+            <div></div>
+          </div>
+        </section>
       </main>
     </>
   );
