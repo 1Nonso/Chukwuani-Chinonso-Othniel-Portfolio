2a8909762bd5a42fa3b2e5ba5b4faa29a102d666
Othniel <chukchi2345@gmail.com>
2024-12-27 06:54:46 +0100

    Modified About Section, Added a description of myself

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,49 @@
 const About = () => {
+  const projectEperience = [
+    {
+      id: 1,
+      title: "+25",
+      description: "Project",
+    },
+    {
+      id: 2,
+      title: "+1",
+      description: "Year Experience",
+    },
+  ];
+
   return (
     <>
-      <main id="About" style={{ scrollMarginTop: '80px' }} className="container mx-auto h-64 bg-amber-500 m-4 flex justify-center items-center">
-        <h1 className="text-center text-5xl font-extrabold">About</h1>
+      <main
+        id="About"
+        style={{ scrollMarginTop: "80px" }}
+        className=" px-5 py-5 text-[#474306]"
+      >
+        <section className="border-2 border-[#F5EE84] p-5 rounded-lg">
+          <p className="mb-5 text-xl">
+            Welcome! I'm Chukwuani Chinonso Othniel, a highly motivated and
+            result-oriented Front-end Web developer with an ability to translate
+            creative concepts into functional and engaging web experiences. I'm
+            a results-oriented with a focus on delivering high-quality Websites
+            and Web-Apps that meet client needs and exceed expectations. I'm
+            always eager to collaborate and contribute to successful web
+            development initiatives.
+          </p>
+          <section className="flex justify-between w-64">
+            {projectEperience.map((project) => {
+              const { id, title, description } = project;
+              return (
+                <>
+                  <div key={id} className="flex flex-col items-start">
+                    <p className="text-4xl">{title}</p>
+                    <p>{description}</p>
+                  </div>
+                </>
+              );
+            })}
+          </section>
+        </section>
+        <section></section>
       </main>
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,21 +9,21 @@ const Home = () => {
         style={{ scrollMarginTop: "80px" }}
         className="flex flex-col justify-between items-center px-7 pt-3"
       >
-        <p className=" text-5xl md:text-8xl welcome">Welcome</p>
+        <p className=" text-5xl md:text-6xl welcome">Welcome</p>
         <section className="flex flex-col-reverse justify-between items-center lg:flex-row px-7 pt-8 w-full">
           <div>
             <p className="text-1xl lg:text-2xl">
               Hello, I'm
-              <strong> Chukwuani Chinonso Othniel,</strong>
+              <strong> Othniel,</strong>
             </p>
-            <h1 className="text-3xl lg:text-8xl">
-              Front-end <br /> Web Developer
+            <h1 className="text-3xl lg:text-7xl">
+              Front-end Web Developer
             </h1>
             <p className=" text-1xl lg:text-2xl mt-1 lg:mt-5">
               +1 Year Experience. Based in Enugu City, Nigeria.
             </p>
           </div>
-          <div className="mb-7 sm:mb-0 w-[80%] md:w-[50%] lg:w-96">
+          <div className="mb-7 sm:mb-0 w-[60%] md:w-[50%] lg:w-96">
             <img
               src={profilePhoto}
               alt="Image of Myself"
@@ -36,6 +36,7 @@ const Home = () => {
             />
           </div>
         </section>
+        <p className="mt-10 mb-7 text-2xl welcome">Web Experiences, Thoughtfully Coded</p>
       </main>
     </>
   );
