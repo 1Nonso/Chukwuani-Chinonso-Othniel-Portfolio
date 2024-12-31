0e78f084dbffa3ce61e593cac9ce066beec3e1a8
Othniel <chukchi2345@gmail.com>
2024-12-31 19:03:39 +0100

    I am testing how my contact section would look like

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,21 @@
 const Contact = () => {
   return (
     <>
-      <main id="Contact" style={{ scrollMarginTop: '80px' }} className="container mx-auto h-64 bg-orange-400 m-4 flex justify-center items-center">  
-        <h1 className="text-center text-5xl font-extrabold">Contact</h1>
+      <main id="Contact" style={{ scrollMarginTop: "80px" }} className="">
+        <h1 className="text-4xl">Let's Connect</h1>
+        <div>
+          <p>
+            I'm always excited to connect with fellow developers, potential
+            collaborators, and anyone interested in web development. Whether you
+            have a project in mind or just want to say hello, feel free to reach
+            out!
+          </p>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
       </main>
     </>
   );
