fbffe22a17b356f44fa16c04f2cfb1e68f587565
Othniel <chukchi2345@gmail.com>
2024-12-31 23:59:16 +0100

    Modified work section, removed all element

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,9 +4,7 @@ const Work = () => {
       <main
         id="Work"
         style={{ scrollMarginTop: '80px' }}
-        className="container mx-auto h-64 bg-red-500 m-4 flex justify-center items-center"
       >
-        <h1 className="text-center text-5xl font-extrabold">Work</h1>
       </main>
     </>
   );
