038b05fe34c15c5fa9850c01cc6cb1a746d55a65
Othniel <chukchi2345@gmail.com>
2024-12-31 23:56:55 +0100

    Modified contact section, corrected some positioning

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import call_me from "../assets/call_me.svg";
 import email_me from "../assets/email_me.svg";
-import headerFont from "../assets/IndieFlower-Regular.ttf";
+import headerFont from "../assets/GiveYouGlory-Regular.ttf";
 import bodyFont from "../assets/Tangerine-Bold.ttf";
 
 const Contact = () => {
@@ -8,25 +8,25 @@ const Contact = () => {
     <>
       <main
         id="Contact"
-        style={{ scrollMarginTop: "80px" }}
-        className="p-4 font-[headerFont] text-[#474306]"
+        style={{ scrollMarginTop: "80px", fontFamily: "headerFont" }}
+        className="p-4 text-[#474306]"
       >
         <h1 className="text-4xl font-[bodyFont]">Let's Connect</h1>
-        <div>
-          <p className="text-xl">
+        <div className="flex flex-row items-center justify-between">
+          <p className="text-xl w-[60%]">
             I'm always excited to connect with fellow developers, potential
             collaborators, and anyone interested in web development. Whether you
             have a project in mind or just want to say hello, feel free to reach
             out!
           </p>
-          <ul className="flex flex-wrap justify-evenly mt-5 w-full">
-            <li className="flex items-center justify-between w-[19.5rem] my-2">
+          <ul className="flex flex-col justify-evenly ml-6 mt-5 w-[40%]">
+            <li className="flex items-center justify-between w-[19.5rem] my-3">
               <img
                 className="bg-gray-400 p-1 rounded-md"
                 src={call_me}
                 alt=""
               />
-              <p>Email: chukwuaniOthniel967@gmail.com</p>
+              <p>Email: chukwuaniothniel967@gmail.com</p>
             </li>
             <li className="flex items-center justify-between w-[17rem] my-3">
               <img
