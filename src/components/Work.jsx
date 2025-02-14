5f2e61f427dc0d07cf2497db9b94329b5996e331
Othniel <chukchi2345@gmail.com>
2025-02-14 16:15:52 +0100

    Modified preview responsive display

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -40,11 +40,11 @@ const Work = () => {
         style={{ scrollMarginTop: "80px" }}
       >
         <h1 className="text-4xl mb-10">My Projects</h1>
-        <section className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <section className="grid grid-col-1 md:grid-cols-2 gap-6">
           {projectData.map((project) => {
             const { name, url, id } = project;
             return (
-              <div key={id} className="projectBorder rounded-3xl pt-4">
+              <div key={id} className="projectBorder lg:w-[30rem] rounded-3xl pt-4">
                 <PreviewPhoto url={url} />
                 <div className="flex flex-col ml-6 my-4">
                   <h2 className="text-2xl">{name}</h2>
